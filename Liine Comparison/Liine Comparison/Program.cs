be08d929f0561fe7e1c2f1f25db2f6b6d0b56dd5
ruchi06 <ruchi06@gmail.com>
2023-01-03 08:16:44 +0530

    Calculate to line

---
+++ Liine Comparison/Liine Comparison/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Liine_Comparison
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
